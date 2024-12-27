43a55d7284e006b0615ffc7642f2ca9d46fd80de
Resh-12 <reshmabhat198@gmail.com>
2024-12-27 15:30:44 +0530

    second commit for contact details

--- src/portfolio.js
+++ src/portfolio.js
@@ -341,8 +341,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+91-0000000000",
-  email_address: "saadpasta70@gmail.com",
+  number: "+91-8495831191",
+  email_address: "reshmabhat198@gmail.com",
   display : true
 };
 
